906f4868dc0820b7cedae8f73c0f4f74281e9b81
perso <elisa.github@gmail.com>
2024-01-19 22:42:32 +0100

    add loading page

---
+++ src/components/loading_page/loading.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './loading.css';
+
+const LoadingScreen = () => {
+
+    return (
+        <div className="loading-screen">
+        <div className="logo">YoRHa</div>
+          <p>For the Glory of Mankind</p>
+          <h1>LOADING</h1>
+          <h2>- BOOTING SYSTEM...</h2>
+          <ul className="system-check">
+            <li>Commencing System Check</li>
+            <li>Memory Unit: Green</li>
+            <li>Initializing OS: Arch Linux</li>
+            <li>Loading Steam Profile Data</li>
+            <li>Vitals: Green</li>
+            <li>Remaining MP: 100%</li>
+            <li>CPU Temperature: Normal</li>
+            <li>Fan Speed: Normal</li>
+            <li>Activating G</li>
+          </ul>
+        </div>
+      );
+    };
+    
+    export default LoadingScreen;
--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,29 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import TextComponent from './../components/text_start/text';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import LoadingScreen from '../components/loading_page/loading'; 
 import GlitchText from '../components/glicht/glicht';
+import TextComponent from './../components/text_start/text';
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleStartClick = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+      navigate('/profile'); 
+    }, 1100); 
+  };
+
   return (
     <div className="custom-container">
       <GlitchText text="START MENU" />
       <div className="text-component-class">
         <TextComponent/> 
       </div>
-      <div className="button-container">
-        <div className="custom-button">
-          <Link to="/profile">
-            <span className="line-above"></span> 
-            <button className="color_text-button">COMMENCER</button>
-            <span className="line-below"></span> 
-          </Link>
-        </div>
-        <div className="custom-button">
-          <Link to="/settings">
-            <span className="line-above"></span> 
-            <button className="color_text-button">PARAMETTRE</button>
-            <span className="line-below"></span> 
-          </Link>
+      <div>
+        {isLoading && <LoadingScreen />}
+        <div className="button-container">
+          <div className="custom-button">
+            {isLoading ? (
+              <div>Chargement en cours...</div>
+            ) : (
+              <button className="color_text-button" onClick={handleStartClick}>
+                COMMENCER
+              </button>
+            )}
+          </div>
+          <div className="custom-button">
+            <Link to="/settings">
+              <span className="line-above"></span>
+              <button className="color_text-button">PARAMETTRE</button>
+              <span className="line-below"></span>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
