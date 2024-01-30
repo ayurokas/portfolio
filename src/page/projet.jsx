1d24c29796dc24c93d408c635a5a651490fe1051
perso <elisa.github@gmail.com>
2024-01-30 15:38:34 +0100

    add pop up

--- src/page/projet.jsx
+++ src/page/projet.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlitchText from '../components/glicht/glicht';
 import '../css/projet.css';
 import imgprojet from '../assets/machine_head.png'; 
+import Cross from '../assets/close.png';
+
 
 
 const saves = [
@@ -10,99 +12,120 @@ const saves = [
     date: '30/12/2022', 
     playTime: '730:45:25', 
     langage : 'html, css',
-    imagePath: imgprojet
+    title :'Transformez une maquette en site web avec HTML & CSS',
+    description: 'Ce projet consiste à créer un site Web fictif de voyage qui permettra aux utilisateurs de découvrir des destinations exotiques et de planifier leurs aventures.\nL\'objectif est de mettre en pratique les compétences en développement web acquises en cours.\nVous devrez concevoir et coder les pages du site, y compris la page d\'accueil, les pages de destination, et la page de planification du voyage pour les versions desktop, mobile et tablette.\nLe projet inclut également la création de fichiers HTML, CSS et l\'intégration d\'images pour illustrer les destinations.\nCe projet d\'école sera évalué en fonction de la qualité de la conception de l\'interface utilisateur, de la mise en page, de la convivialité, et de la conformité aux bonnes pratiques de développement web.',
+    imagePath: imgprojet,
+    url :'https://ayurokas.github.io/OhMyFood/'
   },
   { 
     name: 'OhMyFood', 
     date: '30/12/2022', 
     playTime: '730:45:25', 
     langage : 'html, css, javascript',
-    imagePath: imgprojet
+    imagePath: imgprojet,
+    description: 'Ce projet consiste à créer un site Web fictif de voyage qui permettra aux utilisateurs de découvrir des destinations exotiques et de planifier leurs aventures.\nL\'objectif est de mettre en pratique les compétences en développement web acquises en cours.\nVous devrez concevoir et coder les pages du site, y compris la page d\'accueil, les pages de destination, et la page de planification du voyage pour les versions desktop, mobile et tablette.\nLe projet inclut également la création de fichiers HTML, CSS et l\'intégration d\'images pour illustrer les destinations.\nCe projet d\'école sera évalué en fonction de la qualité de la conception de l\'interface utilisateur, de la mise en page, de la convivialité, et de la conformité aux bonnes pratiques de développement web.',
+
   },
   { 
     name: 'OhMyFood', 
     date: '30/12/2022', 
     playTime: '730:45:25', 
     langage : 'html, css, javascript',
-    imagePath: imgprojet
+    imagePath: imgprojet,
+    description: 'Ce projet consiste à créer un site Web fictif de voyage qui permettra aux utilisateurs de découvrir des destinations exotiques et de planifier leurs aventures.\nL\'objectif est de mettre en pratique les compétences en développement web acquises en cours.\nVous devrez concevoir et coder les pages du site, y compris la page d\'accueil, les pages de destination, et la page de planification du voyage pour les versions desktop, mobile et tablette.\nLe projet inclut également la création de fichiers HTML, CSS et l\'intégration d\'images pour illustrer les destinations.\nCe projet d\'école sera évalué en fonction de la qualité de la conception de l\'interface utilisateur, de la mise en page, de la convivialité, et de la conformité aux bonnes pratiques de développement web.',
+
   },
   { 
     name: 'OhMyFood', 
     date: '30/12/2022', 
     playTime: '730:45:25', 
     langage : 'html, css, javascript',
-    imagePath: imgprojet
+    imagePath: imgprojet,
+    description: 'Ce projet consiste à créer un site Web fictif de voyage qui permettra aux utilisateurs de découvrir des destinations exotiques et de planifier leurs aventures.\nL\'objectif est de mettre en pratique les compétences en développement web acquises en cours.\nVous devrez concevoir et coder les pages du site, y compris la page d\'accueil, les pages de destination, et la page de planification du voyage pour les versions desktop, mobile et tablette.\nLe projet inclut également la création de fichiers HTML, CSS et l\'intégration d\'images pour illustrer les destinations.\nCe projet d\'école sera évalué en fonction de la qualité de la conception de l\'interface utilisateur, de la mise en page, de la convivialité, et de la conformité aux bonnes pratiques de développement web.',
+
   },
   { 
     name: 'OhMyFood', 
     date: '30/12/2022', 
     playTime: '730:45:25', 
     langage : 'html, css, javascript',
-    imagePath: imgprojet
+    imagePath: imgprojet,
+    description: 'Ce projet consiste à créer un site Web fictif de voyage qui permettra aux utilisateurs de découvrir des destinations exotiques et de planifier leurs aventures.\nL\'objectif est de mettre en pratique les compétences en développement web acquises en cours.\nVous devrez concevoir et coder les pages du site, y compris la page d\'accueil, les pages de destination, et la page de planification du voyage pour les versions desktop, mobile et tablette.\nLe projet inclut également la création de fichiers HTML, CSS et l\'intégration d\'images pour illustrer les destinations.\nCe projet d\'école sera évalué en fonction de la qualité de la conception de l\'interface utilisateur, de la mise en page, de la convivialité, et de la conformité aux bonnes pratiques de développement web.',
+
   },
   { 
     name: 'OhMyFood', 
     date: '30/12/2022', 
     playTime: '730:45:25', 
     langage : 'html, css, javascript',
-    imagePath: imgprojet
+    imagePath: imgprojet,
+    description: 'Ce projet consiste à créer un site Web fictif de voyage qui permettra aux utilisateurs de découvrir des destinations exotiques et de planifier leurs aventures.\nL\'objectif est de mettre en pratique les compétences en développement web acquises en cours.\nVous devrez concevoir et coder les pages du site, y compris la page d\'accueil, les pages de destination, et la page de planification du voyage pour les versions desktop, mobile et tablette.\nLe projet inclut également la création de fichiers HTML, CSS et l\'intégration d\'images pour illustrer les destinations.\nCe projet d\'école sera évalué en fonction de la qualité de la conception de l\'interface utilisateur, de la mise en page, de la convivialité, et de la conformité aux bonnes pratiques de développement web.',
+
   },
-  { 
-    name: 'OhMyFood', 
-    date: '30/12/2022', 
-    playTime: '730:45:25', 
-    langage : 'html, css, javascript',
-    imagePath: imgprojet
-  },
-  { 
-    name: 'OhMyFood', 
-    date: '30/12/2022', 
-    playTime: '730:45:25', 
-    langage : 'html, css, javascript',
-    imagePath: imgprojet
-  },
-  { 
-    name: 'OhMyFood', 
-    date: '30/12/2022', 
-    playTime: '730:45:25', 
-    langage : 'html, css, javascript',
-    imagePath: imgprojet
-  },
-  { 
-    name: 'OhMyFood', 
-    date: '30/12/2022', 
-    playTime: '730:45:25', 
-    langage : 'html, css, javascript',
-    imagePath: imgprojet
-  },
+  
+
 ];
 
 const SaveScreen = () => {
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleProjectClick = (project) => {
+    setSelectedProject(project);
+  };
+
+  const openProjectInNewTab = (projectUrl) => {
+    window.open(projectUrl, '_blank');
+  };
+
+  const handleCloseClick = () => {
+    setSelectedProject(null);
+  };
   return (
     <>
       <div className="custom-container">
         <GlitchText text="LES PROJETS" />
       </div>
       <div className="save-screen">
-      <div className="scroll-container"> {/* Ce conteneur aura la barre de défilement */}
-        {saves.map((save, index) => (
-          <div className="save-slot" key={index}>
-            <img src={save.imagePath} alt={`${save.name} project`} className="save-image" />
-            <div className="slot-content">
-              <div className="slot-header">
-                <span className="save-name">{save.name}</span>
-                <span className="save-date">{save.date}</span>
+        <div className="scroll-container">
+          {saves.map((save, index) => (
+            <div className="save-slot" key={index} onClick={() => handleProjectClick(save)}>
+              <img src={save.imagePath} alt={`${save.name} project`} className="save-image" />
+              <div className="slot-content">
+                <div className="slot-header">
+                  <span className="save-name">{save.name}</span>
+                  <span className="save-date">{save.date}</span>
+                </div>
+                <div className="slot-details">
+                  <span className="slot-language">{`Langage: ${save.langage}`}</span>
+                  <span className="play-time">{`Play Time: ${save.playTime}`}</span>
+                </div>
               </div>
-              <div className="slot-details">
-                <span className="slot-language">{`Langage: ${save.langage}`}</span>
-                <span className="play-time">{`Play Time: ${save.playTime}`}</span>
-              </div>
+            </div>  
+          ))}
+        </div>
+        {selectedProject && (
+          <div className="project-details">
+            <div className="project-title">
+              {selectedProject.name}
+              <img src={Cross} alt="Close" className="close-button" onClick={handleCloseClick} />
             </div>
-          </div>  
-        ))}
-        </div>  
+            <div className="project-content">
+              <p className="project-intitule">{selectedProject.title}</p>
+              <p className="project-desciption">
+                {selectedProject.description.split('\n').map((paragraph, index) => (
+                  <React.Fragment key={index}>
+                    {paragraph}
+                    <br /> 
+                  </React.Fragment>
+                ))}
+              </p>
+              <button className="project-button" onClick={() => openProjectInNewTab(selectedProject.url)}>Voir le projet</button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
 };
+
 export default SaveScreen;
