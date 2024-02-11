4ec19b9c3f967ac5b6159ed5d718ac14c88de219
perso <elisa.github@gmail.com>
2024-02-11 21:42:42 +0100

    Update menu.jsx

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -27,7 +27,10 @@ function Menu() {
                     <div className="navbar-brand" onClick={() => { window.location.href = '/'; }}>
                         <img src={arrow} alt="Back to home"/>
                     </div>
-                    <div className="collapse navbar-collapse justify-content-center">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className={`nav-item ${clickedLink === '/profile' ? 'active' : ''}`}>
                                 <NavLink to="/profile" className={getNavLinkClass('/profile')} onClick={(e) => handleNavLinkClick(e, '/profile')}>
